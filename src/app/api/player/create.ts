ca4847c4aaf436347c9d5a8a499a84a4713e3a9b
cameroncrook <cameronbcrook@gmail.com>
2025-09-29 15:08:09 -0600

    Player endpoint additions

--- src/app/api/player/create.ts
+++ src/app/api/player/create.ts
@@ -9,14 +9,15 @@ export async function createPlayer(request: Request) {
     }
     
     try {
-        const {} = await request.json();
+        const {first_name, last_name, date_of_birth, address, school, grade_level, teacher, shirt_size, p1_first_name, p1_last_name, p1_email, p1_phone, p2_first_name, p2_last_name, p2_email, p2_phone, emergency_name, emergency_phone, emergency_relation, medical_conditions, emergency_care_consent, liability_waiver, photo_permission, volunteer_interest, walk_home_permission, travel_permission, general_waiver_signed, signature} = await request.json();
 
+        console.log(p1_first_name, medical_conditions, travel_permission, liability_waiver)
         // Add player to db
-        const player = await prisma.player.create({
-            data: {
+        // const player = await prisma.player.create({
+        //     data: {
 
-            },
-        });
+        //     },
+        // });
 
         return new Response(JSON.stringify({ message: 'Player created'}), {
             status: 200,
