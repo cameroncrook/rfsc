a53c9a81be3d00a9238609728ba3d813de45b5de
cameroncrook <cameronbcrook@gmail.com>
2025-10-04 22:07:32 -0600

    Ham menu

--- src/components/mainNav.tsx
+++ src/components/mainNav.tsx
@@ -1,12 +1,22 @@
+'use client'
+
 import React from 'react';
 import Link from 'next/link';
+import { useState } from 'react';
 
 type MainNaveProps = {
     lightMode?: boolean; 
 };
 
-const MainNav: React.FC<MainNaveProps> = ({ lightMode }) => (
-    <div className="container mx-auto px-6 py-4 flex justify-between items-center">
+const MainNav: React.FC<MainNaveProps> = ({ lightMode }) => {
+    const [hamIsOpen, setHamIsOpen] = useState(false);
+
+    function toggleHam() {
+        setHamIsOpen(!hamIsOpen);
+    }
+
+    return (
+    <div className="container relative mx-auto px-6 py-4 flex justify-between items-center">
         <div className="flex items-center">
             <span className="material-icons text-primary text-3xl">sports_soccer</span>
             <span className={`ml-2 text-xl font-bold ${lightMode ? 'text-gray-200' : 'text-gray-800'}`}>Rigby Soccer</span>
@@ -20,10 +30,22 @@ const MainNav: React.FC<MainNaveProps> = ({ lightMode }) => (
         </nav>
 
         <Link className="bg-primary/90 text-white px-6 py-2 rounded-full hover:bg-primary transition-colors hidden md:block" href="/register">Register to Play!</Link>
-        <button className="md:hidden">
+        <button onClick={toggleHam} className="md:hidden">
             <span className={`material-icons ${lightMode ? 'text-gray-200' : 'text-gray-800'}`}>menu</span>
         </button>
+
+        {hamIsOpen && (
+            <div className='absolute left-0 top-full bg-gray-200 text-primary w-full text-center py-4 space-y-4 md:hidden'>
+                <Link className="block" href="/">Home</Link>
+                <Link className="block" href="/schedule">Schedule</Link>
+                <Link className="block" href="/contact">Contact</Link>
+                <Link className="block" href="/about">About</Link>
+
+                <Link className="bg-primary text-white mx-6 px-6 py-2 rounded-full block" href="/register">Register to Play!</Link>
+            </div>
+        )}
     </div>
-);
+    );
+};
 
 export default MainNav;
